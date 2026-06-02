d1371da3c7652875ad46c84ef5f6b9d2c22997cc
ma332398 <ma332398@gmail.com>
2026-06-02 19:01:37 +0900

    12p
    ma3323 <12>

---
+++ c12/cms/article.php
@@ -0,0 +1,71 @@
+<?php
+// 슬라이드 53
+declare(strict_types = 1); // 엄격한 타입 사용
+require_once 'includes/database-connection.php'; // PDO 객체
+require_once 'includes/functions.php';
+
+// -----------
+$sql = "SELECT a.id, a.title, a.summary, a.category_id, a.member_id,
+               c.name AS category,
+               CONCAT(m.forename, ' ', m.surname) AS author,
+               i.file AS image_file,
+               i.alt AS image_alt
+          FROM article AS a
+          JOIN category AS c ON a.category_id = c.id
+          JOIN member AS m ON a.member_id = m.id
+          LEFT JOIN image AS i ON a.image_id = i.id
+          WHERE a.published = 1
+          ORDER BY a.id DESC
+          LIMIT 6;"; // 최근 기사 가져오는 SQL
+$article = pdo($pdo, $sql)->fetch(); // 기사 1개 불러오기
+
+if (!$article) {
+    include 'page-not-found.php';
+    exit;
+}
+
+$sql = "SELECT id, name FROM category WHERE navigation = 1;";
+$navigation = pdo($pdo, $sql)->fetchAll();
+
+$section     = $article['category_id'];
+$title       = $article['title'];
+$description = $article['summary'];
+?>
+
+<?php include 'includes/header.php'; ?>
+
+<main class="article container" id="content">
+
+    <section class="image">
+        <img src="uploads/<?= html_escape($article['image_file'] ?? 'blank.png') ?>"
+             alt="<?= html_escape($article['image_alt'] ?? '') ?>">
+    </section>
+
+    <section class="text">
+
+        <h1><?= html_escape($article['title']) ?></h1>
+
+        <div class="date">
+            <?= format_date($article['created']) ?>
+        </div>
+
+        <div class="content">
+            <?= html_escape($article['content']) ?>
+        </div>
+
+        <p class="credit">
+            Posted in
+            <a href="category.php?id=<?= $article['category_id'] ?>">
+                <?= html_escape($article['category']) ?>
+            </a>
+            by
+            <a href="member.php?id=<?= $article['member_id'] ?>">
+                <?= html_escape($article['author']) ?>
+            </a>
+        </p>
+
+    </section>
+
+</main>
+
+<?php include 'includes/footer.php'; ?>
